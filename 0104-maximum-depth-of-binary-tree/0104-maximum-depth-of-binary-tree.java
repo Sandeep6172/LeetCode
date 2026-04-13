30fccc32d592de74423d2c16649d2727a3417596
Sandeep Kumar <sandeep.kumar.cs27@iilm.edu>
2026-04-13 10:38:09 +0530

    Time: 0 ms (100%), Space: 44.4 MB (70.13%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -14,24 +14,14 @@
  * }
  */
 class Solution { 
-    int res=0;
     public int maxDepth(TreeNode root) {
-        dfs(root,0);
-        return res;
-
         
-    }
-    void dfs(TreeNode root,int c){
-        if(root==null){
-            return;
+    
+        if (root == null) {
+            return 0;
         }
-        c++; 
-        res=Math.max(res,c);
-        if(root.left!=null){
-            dfs(root.left,c);
-        }
-        if(root.right!=null){
-            dfs(root.right,c);
-        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return 1 + Math.max(leftDepth, rightDepth);
     }
 }
